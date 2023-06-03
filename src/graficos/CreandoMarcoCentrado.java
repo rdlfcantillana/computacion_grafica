3731ad9074a4abc260f400cabbb55f3e70b1a826
rdlfcantillana <98615619+rdlfcantillana@users.noreply.github.com>
2023-06-03 13:18:26 -0400

    first commit

---
+++ src/graficos/CreandoMarcoCentrado.java
@@ -0,0 +1,35 @@
+package graficos;
+import java.awt.*;
+import javax.swing.*;
+
+public class CreandoMarcoCentrado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		MarcoCentrado miMarco  = new MarcoCentrado();
+		
+		miMarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		miMarco.setVisible(true);
+		
+		
+	}
+}
+class MarcoCentrado extends JFrame {
+	
+	public MarcoCentrado() {
+		
+	Toolkit miPantalla = Toolkit.getDefaultToolkit();		
+	
+	Dimension tamanioPantalla = miPantalla.getScreenSize();
+	
+	int alturaPantalla = tamanioPantalla.height;
+	
+	int anchoPantalla = tamanioPantalla.width;
+	
+	setSize(anchoPantalla/2, alturaPantalla/2 );
+	
+	setLocation(anchoPantalla/4, alturaPantalla/4 );
+	}
+}

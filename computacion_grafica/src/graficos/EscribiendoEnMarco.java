118f4f9f2da90f69c4ad5b572464fc89c18e649a
Rodolfo Cantillana <98615619+rdlfcantillana@users.noreply.github.com>
2023-06-10 13:25:55 -0400

    Add files via upload

--- computacion_grafica/src/graficos/EscribiendoEnMarco.java
+++ computacion_grafica/src/graficos/EscribiendoEnMarco.java
@@ -1,10 +1,33 @@
 package graficos;
-
+import java.awt.Graphics;
+import javax.swing.*;
 public class EscribiendoEnMarco {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		MarcoConTexto mimarco = new MarcoConTexto();
+		mimarco.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
 	}
 
+	class MarcoConTexto extends JFrame{
+		public MarcoConTexto() {
+		setVisible(true);
+		setSize(600, 450);
+		setLocation(400, 200);
+		setTitle("Primer Texto");
+		
+		Lamina miLamina = new Lamina();
+		add (miLamina);
 }
+		
+		class Lamina extends JPanel{
+		public void paintComponent (Graphics g){
+			
+			super.paintComponent(g);
+			g. drawString ("Clase de Computacion Grafica", 100, 100 );
+			
+		}
+	
+	}
+}}
